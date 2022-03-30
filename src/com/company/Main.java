5796dee09d642e113b2dd497342cb7c56b706193
nastyafallen <nastyafallen@rambler.ru>
2022-03-31 00:46:47 +0300

    Мой первый коммит

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Java. My first homework
+ *
+ * @author Anastasia Khorevich
+ * @version 30.03.2022
+ */
+public class Main {
+    public Main () {
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello, SkyPro Team!"); // write your code here
+    }
+}
